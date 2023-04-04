22ea91b917cc8cb49349576f5fe6a20439bc1358
Valeria Balducci <v_balducci@web.de>
2023-04-04 17:22:45 +0200

    fix: bug fix map

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,7 +11,7 @@ export const MainView = () => {
     fetch("https://indieflix.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => {
-        const moviesFromApi = data.docs.map((doc) => {
+        const moviesFromApi = data.map((doc) => {
           return {
             id: doc._id,
             title: doc.Title,
