9679ebcee538fd3d638f82ac03e39450169ed823
Valeria Balducci <v_balducci@web.de>
2023-04-12 00:25:10 +0200

    feat: add Container component

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,19 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.scss";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
+import { Container } from "react-bootstrap";
+
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
   return (
-      <div className="my-flix">
-          <MainView />
-      </div>
-  );
+    <Container>
+    <MainView />
+  </Container>
+);
   };
 
 // Finds the root of your app
