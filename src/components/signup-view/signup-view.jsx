c754ffddd6677cf3dcb8dfe28bdc38a89e86b2d6
Valeria Balducci <v_balducci@web.de>
2023-04-13 22:22:35 +0200

    feat: correct indentation

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -36,46 +36,46 @@ import { Button, Form } from "react-bootstrap";
 
    return (
      <Form onSubmit={handleSubmit}>
-   <Form.Group controlId="formUsername">
-     <Form.Label>Username:</Form.Label>
-     <Form.Control
-       type="text"
-       value={username}
-       onChange={(e) => setUsername(e.target.value)}
-       required
-       minLength="3" 
-     />
-     </Form.Group>
-     <Form.Group controlId="formPassword">
-      <Form.Label>Password:</Form.Label>
-      <Form.Control
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-    </Form.Group>
-       <Form.Group controlId="formEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-      />
-    </Form.Group>
-    <Form.Group controlId="formBirthday">
-      <Form.Label>Birthday:</Form.Label>
-      <Form.Control
-        type="edatemail"
-        value={birthday}
-        onChange={(e) => setBirthday(e.target.value)}
-        required
-      />
-    </Form.Group>
-       <Button variant="primary" type="submit">
-     Submit
-   </Button>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            minLength="3" 
+          />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group controlId="formEmail">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+          />
+        </Form.Group>
+        <Form.Group controlId="formBirthday">
+          <Form.Label>Birthday:</Form.Label>
+          <Form.Control
+            type="edatemail"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+           Submit
+        </Button>
      </Form>
    );
  };
